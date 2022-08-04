bb590c9f663c822e596bd92cde037e83e4861ef5
Tsvetelina Sotirova <105795994+Tsvetelina-S@users.noreply.github.com>
2022-08-04 11:22:52 +0300

    Add files via upload

---
+++ Animals/Animal.java
@@ -0,0 +1,24 @@
+package Animals;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public abstract String explainSelf();
+
+
+}
