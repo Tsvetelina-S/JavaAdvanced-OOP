005c285aa3f6d48d3ac89288b15f29d5f6098fbd
Tsvetelina Sotirova <105795994+Tsvetelina-S@users.noreply.github.com>
2022-08-06 15:11:50 +0300

    Delete Abstraction directory

--- Abstraction/CardRank/CardRanks.java
+++
@@ -1,9 +0,0 @@
-package Abstraction.CardRank;
-
-public enum CardRanks {
-    ACE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING;
-
-    public String toString(){
-        return String.format("Ordinal value: %d; Name value: %s",ordinal(),name());
-    }
-}
--- Abstraction/TrafficLights/Light.java
+++
@@ -1,9 +0,0 @@
-package Abstraction.TrafficLights;
-
-public enum Light {
-    RED,GREEN,YELLOW;
-
-//    public static Light parse(String s){
-//        return Light.valueOf(s);
-//    }
-}
